d79704ff6f3bc307dc4305c7b265b49e2e114c48
unknown <tima.gizatullin.01@mail.ru>
2019-12-03 19:52:02 +0500

    feat: add header in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('hello', function(done) {
   console.log('Привет, Мир');
@@ -14,4 +15,20 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('minify_css_task',[other_task], function() {
+  return gulp.src('css/mehr.css')
+    .pipe(cleanCSS({debug: true}, function(details) {
+      compatibility: 'ie8';
+      console.log('Original size of '+ details.name + ': ' + details.stats.originalSize);
+      console.log('Minified size of '+ details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
