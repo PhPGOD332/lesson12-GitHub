a9fcb867b1e4c7dce47f0eaf85eb9be53d36c51c
unknown <tima.gizatullin.01@mail.ru>
2019-12-05 17:43:34 +0500

    fix: fixed buttons in hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 
-gulp.task('hello', function(done) {
-  console.log('Привет, Мир');
-  done();
-});
-
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return gulp.src(".sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
 
 gulp.task('minify_css_task',[other_task], function() {
   return gulp.src('css/mehr.css')
