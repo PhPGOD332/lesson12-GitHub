864ca6919c2b7b89ee31eb11fcd79953fe57b66c
unknown <tima.gizatullin.01@mail.ru>
2019-12-05 19:42:41 +0500

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
 
 // Static server
 function bs() {
@@ -17,26 +16,10 @@ function bs() {
 };
 
 function serveSass() {
-  return gulp.src(".sass/*.sass")
+  return src(".sass/*.sass")
       .pipe(sass())
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
-
-gulp.task('minify_css_task',[other_task], function() {
-  return gulp.src('css/mehr.css')
-    .pipe(cleanCSS({debug: true}, function(details) {
-      compatibility: 'ie8';
-      console.log('Original size of '+ details.name + ': ' + details.stats.originalSize);
-      console.log('Minified size of '+ details.name + ': ' + details.stats.minifiedSize);
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
