2f8a254364ab101a7d603bcdd2c40601964f2662
unknown <tima.gizatullin.01@mail.ru>
2019-12-20 16:21:11 +0500

    fix: fix modal window

--- js/main.js
+++ js/main.js
@@ -46,16 +46,6 @@ $(document).ready(function () {
 
   new WOW().init();
 
-  $('.estm-phone-app img').each(function () {
-    var imagePos = $(this).offset().top;
-
-    var topOfWindow = $(window).scrollTop();
-    if (imagePos < topOfWindow + 500) {
-        $(this).addClass("animated slideInUp")
-    }
-  });
-  wow.init();
-
   // Валидация формы
   $('.modal__form').validate({
     errorClass: "invalid",
