9eba3ee9e0f87cfdafe4d7e8777e183c48ec8243
unknown <tima.gizatullin.01@mail.ru>
2020-01-06 20:52:22 +0500

    fix: add adaptive and optimization

---
+++ dist/js/main.js
@@ -0,0 +1,192 @@
+/* document.addEventListener('DOMContentLoaded', function(event) {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle = modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal)
+  });
+
+  closeBtn.addEventListener('click', switchModal)
+
+}); */
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination')
+
+  next.css('left', prev.width() + 30 + bullets.width() + 30)
+  bullets.css('left', prev.width() + 30)
+
+  new WOW().init();
+
+  // Валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // Строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // Правило объект
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // Сообщения
+    errorElement: "div",
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв",
+        maxlength: "Имя не больше 15 букв"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно введие Email",
+        email: "Введите в формате: name@domain.com"
+      }
+    }
+  });
+  $('.footer__form').validate( {
+    errorClass: "invalid",
+    rules: {
+      // Строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // Правило объект
+      userQuestion: {
+        required: true,
+        minlength: 20,
+        maxlength: 300
+      },
+      userQuestion: {
+        required: true,
+        text: true
+      }
+    }, // Сообщения
+    errorElement: "div",
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв",
+        maxlength: "Имя не больше 15 букв"
+      },
+      userPhone: "Телефон обязателен",
+      userQuestion: {
+        required: "Обязательно введите вопрос",
+        minlength: "Текст не короче 20 символов",
+        maxlength: "Текст не больше 300 символов"
+      }
+    }
+  });
+
+  // Валидация формы
+  $('.control__form').validate( {
+    errorClass: "invalid",
+    rules: {
+      // Строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+    }, // Сообщения
+    errorElement: "div",
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв",
+        maxlength: "Имя не больше 15 букв"
+      },
+      userPhone: "Телефон обязателен",
+    }
+  });
+
+  $('#modal-form').on('submit', function (event) { 
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "send.php",
+      data: $(this).serialize(),
+      success: function (response) {
+        console.log('Прибыли данные: ' + response);
+        $('#modal-form')[0].reset();
+      },
+      error: function (jqXNR, textStatus,errorThrown) { 
+        console.error(jqXNR + " " + textStatus );
+       }
+    });
+   });
+
+  // Маска для номера телефона 
+
+  $('[type=tel]').mask('+7 (000) 00-00-000', {placeholder: "+7/___/__-__-___"})
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/location.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        })
+
+    myMap.geoObjects
+        .add(myPlacemark)
+});
+});
---
+++ dist/send.php
@@ -0,0 +1,44 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'repairdesign332@gmail.com';                     // SMTP username
+    $mail->Password   = 'Sim4salabin600';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+    $mail->CharSet = 'UTF-8';
+
+    //Recipients
+    $mail->setFrom('repairdesign332@gmail.com', 'Тимур');
+    $mail->addAddress('tima.gizatullin.01@mail.ru');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его/её телефон: ${userPhone}. Его/её почта: ${userEmail}";
+
+    if ($mail->send()) {
+        echo "OK";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+    
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
